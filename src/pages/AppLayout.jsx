dc2cb41659683e3a65d9d4f9e2758e8b18523402
ReiKoka <reikoka1@outlook.com>
2025-02-10 17:41:58 +0100

    Started on AppLayout design

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,10 @@
 function AppLayout() {
   return (
-    <div>
-      AppLayout
+    <div className="bg-background relative flex h-dvh w-dvw items-center justify-center">
+      <div className="bg-primary absolute top-0 left-0 z-0 h-32 w-full"></div>
+      <div className="bg-muted grid-cols-[80px_1fr_2fr] z-10 grid h-[90%] w-full max-w-[1600px] rounded-lg"></div>
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
