456924fe8d69f5ae7ce4e20a0b4dd8fa1b077c16
ReiKoka <reikoka1@outlook.com>
2025-02-21 15:22:16 +0100

    Avatar style fix

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -7,7 +7,7 @@ const NavItem = ({ to, user, icon: Icon }) => {
       <NavLink
         to={to}
         className={({ isActive }) =>
-          `focus-visible:ring-primary dark:focus-visible:ring-offset-muted flex h-full w-full items-center justify-center rounded-full ${isPicture(user?.profilePicture) ? "p-1" : "p-2"} transition-all duration-300 ease-out group-hover:scale-110 group-active:scale-75 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none ${
+          `focus-visible:ring-primary focus-visible:ring-offset-secondary dark:focus-visible:ring-offset-muted flex h-full w-full items-center justify-center rounded-full ${isPicture(user?.profilePicture) ? "p-1" : "p-2"} transition-all duration-300 ease-out group-hover:scale-110 group-active:scale-75 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none ${
             isActive || location.pathname === to
               ? `${user?.profilePicture ? "bg-primary" : "bg-primary"} bg-primary text-primary-foreground`
               : `text-muted-foreground group-hover:text-primary`
