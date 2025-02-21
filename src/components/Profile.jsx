d4cad3b80b176efd1e7986a5070b6f3485decbe7
ReiKoka <reikoka1@outlook.com>
2025-02-21 17:17:25 +0100

    Added button to change profile picture

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../hooks/useAuth";
 import { isPicture } from "../utils/helpers";
 import Input from "./ui/Input";
 import Title from "./ui/Title";
-import { HiUser, HiUserCircle } from "react-icons/hi2";
+import { HiCamera, HiUserCircle } from "react-icons/hi2";
+import Button from "./ui/Button";
 
 function Profile() {
   const { user } = useAuth();
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const [fullName, setFullName] = useState(() => (user ? user?.fullName : ""));
 
@@ -21,13 +25,17 @@ function Profile() {
     }
   }, [user]);
 
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <div className="px-2 py-2 lg:px-4 lg:py-4">
         <Title title="Profile" />
       </div>
       <div className="bg-secondary/40 grid place-items-center py-8">
-        <div className="h-52 max-h-52 w-52 max-w-52 cursor-pointer overflow-hidden rounded-full">
+        <div className="group relative h-52 max-h-52 w-52 max-w-52 cursor-pointer overflow-hidden rounded-full">
           {isPicture(user?.profilePicture) ? (
             <img
               src={`${isPicture(user?.profilePicture) && user?.profilePicture} `}
@@ -37,6 +45,17 @@ function Profile() {
           ) : (
             <HiUserCircle className="fill-muted-foreground block h-52 w-52" />
           )}
+          <Button
+            ref={buttonRef}
+            title="open menu"
+            type="button"
+            variant="icon"
+            className="bg-border/80 text-background dark:text-foreground absolute top-0 left-0 z-20 hidden min-h-52 min-w-52 flex-col gap-2 rounded-full font-bold uppercase group-hover:flex"
+            onClick={handleClick}
+          >
+            <HiCamera className="w-12 h-12" />
+            <span className="max-w-40">Change Profile Picture</span>
+          </Button>
         </div>
       </div>
       <form
